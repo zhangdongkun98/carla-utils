fa5565d17b7b534ce68e7527316cf17a9ae9ba54
zhangdongkun98 <zhangdongkun98@gmail.com>
2020-11-10 18:41:36 +0800

    add run and config

---
+++ carla_utils/config.py
@@ -0,0 +1,22 @@
+import carla
+
+import os, sys
+from os.path import join
+
+print(sys.argv)
+current_path = os.getcwd()
+
+args = ''
+for arg in sys.argv[1:]:
+    args += ' ' + arg
+
+server_path = os.environ['CARLAPATH']
+
+target_path = join(server_path, 'PythonAPI/util/')
+os.chdir(target_path)
+
+cmd = sys.executable + ' ' + 'config.py'
+cmd = cmd + args
+
+print('\nrunning:\n    '+cmd+'\n')
+os.system(cmd)
