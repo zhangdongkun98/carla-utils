8f4b6fa8bf5eba33b574c2e4d18b9e83f71be04b
zhangdongkun98 <zhangdongkun98@gmail.com>
2021-01-20 19:26:39 +0800

    update learning

--- carla_utils/basic/learning.py
+++ carla_utils/basic/learning.py
@@ -3,7 +3,7 @@ import os
 from os.path import join
 import time
 from collections import namedtuple
-
+from tensorboardX import SummaryWriter
 
 PathPack = namedtuple('PathPack', ('log_path', 'save_model_path', 'output_path'))
 
@@ -14,7 +14,7 @@ def create_dir(config, model_name):
             config: need to contain:
                 config.description
     '''
-    dataset_name = model_name + '/' + str(int(time.time())) + '----' + config.description
+    dataset_name = model_name + '/' + str(int(time.time())) + '----' + str(config.description)
     print('create dir: ', dataset_name)
     log_path = join('results', dataset_name, 'log')
     save_model_path = join('results', dataset_name, 'saved_models')
@@ -23,5 +23,8 @@ def create_dir(config, model_name):
     os.makedirs(output_path, exist_ok=True)
     with open(join('results', dataset_name, 'comments'), mode='w', encoding='utf-8') as _: pass
     config.save(join('results', dataset_name))
-    return PathPack(log_path, save_model_path, output_path)
+
+    logger = SummaryWriter(log_dir=log_path)
+    logger.add_text('description', dataset_name, 0)
+    return PathPack(log_path, save_model_path, output_path), logger
 
